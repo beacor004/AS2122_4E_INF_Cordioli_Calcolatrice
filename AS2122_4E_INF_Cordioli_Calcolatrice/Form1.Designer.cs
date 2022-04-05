bcc3ac0a2affd856fdc8bf1431d08da103afae88
beacor004 <99179898+beacor004@users.noreply.github.com>
2022-04-05 16:11:43 +0200

    Bottone Calcola

--- AS2122_4E_INF_Cordioli_Calcolatrice/Form1.Designer.cs
+++ AS2122_4E_INF_Cordioli_Calcolatrice/Form1.Designer.cs
@@ -29,13 +29,33 @@ namespace AS2122_4E_INF_Cordioli_Calcolatrice
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(369, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calcola";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
